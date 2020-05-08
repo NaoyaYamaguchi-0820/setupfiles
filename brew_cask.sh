37b8bdb74edcf838a54b2dcbe5a82c0ebc0a8a1b
NaoyaYamaguchi <55648796+NaoyaYamaguchi-0820@users.noreply.github.com>
2020-05-09 00:04:10 +0900

    第一版作成

---
+++ brew_cask.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+# alfred 高機能なランチャーアプリ。有料。
+brew cask install alfred
+
+# Atom GitHub製のテキストエディタ。
+brew cask install atom
+
+# BetterTouchTool 通称BTT。Macのトラックパッドの機能を拡張する。
+brew cask install bettertouchtool
+
+# Docker Docker for Mac。
+brew cask install docker
+
+# Dropbox 言わずと知れたクラウドストレージ。
+brew cask install dropbox 
+
+# Evernote ノートアプリ。
+brew cask install Evernote
+
+# Hackgen 美しいプログラミング用フォント。Rictyよりもお気に入り。
+brew cask install font-hackgen
+
+# Ricty Diminished プログラミング用フォント。Electron製アプリとの相性が悪い場合あり。
+brew cask install font-ricty-diminished
+
+# Fuwari cmd + shift + 5 で部分切り取りすると、最前面に固定されて表示される。メモ等に便利。
+brew cask install fuwari
+
+# GIMP 高機能画像編集ソフト。
+brew cask install gimp
+
+# Github Githubのクライアントソフト。ローカルリポジトリを気軽に作ったりできる。
+brew cask install github
+
+# Google Chrome Google製のブラウザ。拡張機能が豊富にあるのと、ブックマーク同期が便利。
+brew cask install google-chrome
+
+# Google日本語入力 Google製のIME。予測変換が優秀＆Vimとの相性が良い。
+brew cask install google-japanese-ime
+
+# IINA 軽量＆高機能な動画プレイヤー。長い動画もクラッシュせずサクサク動く神ソフト。
+brew cask install iina
+
+# Java Java。いつの間にか入っていた。
+brew cask install java
+
+# Kindle Amazonの電子書籍リーダー。技術書を開きっぱなしにするときとかに使う。
+brew cask install kindle
+
+# MAMP ローカルにWebサーバーを建てられる。PHPの練習をしたいときなどに便利。
+brew cask install mamp
+
+# Notion Evernoteみたいなノートアプリ。すべてのドキュメントを階層状に保存するのが特徴。
+brew cask install notion
+
+# Sketch 画像編集ソフト。あんまり使ったことない。
+brew cask install sketch
+
+# Skitch Evernote製画像編集アプリ。矢印入れたりモザイクかけたりできる。
+brew cask install skitch
+
+# Slack チャットアプリ。勉強会などではSlackが活躍する。
+brew cask install slack
+
+# SourceTree 日本語にも対応するGitクライアントアプリ。
+brew cask install sourcetree
+
+# Sublime-Text 軽量なテキストエディタ。メモ帳感覚で使う。
+brew cask install sublime-text
+
+# Vagrant 仮想化に使ったりする。
+brew cask install Vagrant
+
+# Visual Studio Code Microsoft製テキストエディタ。超高機能の割に軽いので、個人的に最強エディタ。
+brew cask install visual-studio-code
+
+# XMind マインドマップアプリ。思考整理に使う。
+brew cask install xmind
